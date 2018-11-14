03634b7624dd9d26ba4fea8f3982e23bbc8bcb3d
juldhyne <julien.dhyne@gmail.com>
2018-11-14 13:58:19 +0100

    :wrench: Working with MySQL 8

--- petclinic-core/src/resources/db-migration/sprint_1/database_user_petclinic.sql
+++ petclinic-core/src/resources/db-migration/sprint_1/database_user_petclinic.sql
@@ -2,5 +2,6 @@
 
 --changeset tricarico:1
 
-CREATE DATABASE petclinic CHARACTER SET utf8 COLLATE utf8_general_ci;
-GRANT SELECT, INSERT, UPDATE, DELETE ON petclinic.* to 'petclinic'@'%' IDENTIFIED BY 'petclinic';
+CREATE DATABASE IF NOT EXISTS petclinic CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER IF NOT EXISTS 'petclinic'@'%' IDENTIFIED BY 'petclinic';
+GRANT SELECT, INSERT, UPDATE, DELETE ON petclinic.* to 'petclinic'@'%';
